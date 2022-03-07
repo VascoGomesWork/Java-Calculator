5229ac05bca3f529deca2af3fa3fc0056530bb9b
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2022-03-07 22:11:14 +0000

    Operation Tests added

--- app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.example.calculator.operations.OperationChooser;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -22,5 +24,19 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.calculator", appContext.getPackageName());
+    }
+
+    /*@Test
+    public void addTest(){
+
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "2+2";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("4", result);
+    }*/
+
+    @Test
+    public void test(){
+        assertEquals("1", "1");
     }
 }
--- app/src/androidTest/java/com/example/calculator/operations/OperationChooserTest.java
+++ app/src/androidTest/java/com/example/calculator/operations/OperationChooserTest.java
@@ -1,18 +1,84 @@
 package com.example.calculator.operations;
 
-import junit.framework.TestCase;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class OperationChooserTest extends TestCase {
+import static org.junit.Assert.*;
+
+import com.example.calculator.operations.OperationChooser;
+@RunWith(AndroidJUnit4.class)
+public class OperationChooserTest {
 
     @Test
     public void addTest(){
 
         OperationChooser operationChooser = new OperationChooser();
-        String operationString = "2+2";
-        double result = operationChooser.chooseOperation(operationString, operationString.length());
-        assertEquals(4.0, result);
+        String operationString = "2+2=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("4.0", result);
+    }
+
+    @Test
+    public void subtractTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "2-2=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("0.0", result);
+    }
+
+    @Test
+    public void multiplyTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "2*2=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("4.0", result);
+    }
+
+    @Test
+    public void divideTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "4-2=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("2.0", result);
+    }
+
+    @Test
+    public void prioritizingOperationStartTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "2*4-2+1=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("7.0", result);
+    }
+
+    @Test
+    public void prioritizingOperationMiddleTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "1+4*2-2=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("7.0", result);
+    }
+
+    @Test
+    public void prioritizingOperationEndTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "3-2+2*4=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("9.0", result);
+    }
+
+    @Test
+    public void multiplePrioritizingOperationTest(){
+        OperationChooser operationChooser = new OperationChooser();
+        String operationString = "2*3-2+2*4=";
+        String result = "" + operationChooser.chooseOperation(operationString, operationString.length());
+        assertEquals("12.0", result);
     }
 
 }
